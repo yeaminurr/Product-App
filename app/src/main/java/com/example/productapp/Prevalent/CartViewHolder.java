22af9f876f2de5585911d95ebb434359a3fdbba9
Yeaminur Rahman <yeaminur.rahman1@gmail.com>
2022-03-31 22:14:44 +0600

    first commit

---
+++ app/src/main/java/com/example/productapp/Prevalent/CartViewHolder.java
@@ -0,0 +1,29 @@
+package com.example.productapp.Prevalent;
+
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.productapp.Interface.ItemClickListner;
+import com.example.productapp.R;
+
+public class CartViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public TextView txtProductName,txtProductPrice,txtProductQuantity;
+    private ItemClickListner itemClickListner;
+
+    public CartViewHolder(View itemView) {
+        super(itemView);
+        txtProductName = itemView.findViewById(R.id.cart_product_name);
+        txtProductPrice = itemView.findViewById(R.id.cart_product_price);
+        txtProductQuantity = itemView.findViewById(R.id.cart_product_quantity);
+    }
+
+    @Override
+    public void onClick(View view) {
+       itemClickListner.onClick(view,getAdapterPosition(),false);
+    }
+
+    public void setItemClickListner(ItemClickListner itemClickListner) {
+        this.itemClickListner = itemClickListner;
+    }
+}
---
+++ app/src/main/java/com/example/productapp/hudai.java
@@ -0,0 +1,16 @@
+package com.example.productapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class hudai extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hudai);
+
+    }
+}
